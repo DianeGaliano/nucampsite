506fd8b886878e3d1f7a5ec4008d8a8b9a8249b1
Diane Galiano <dianegaliano09@gmail.com>
2023-04-28 00:13:58 -0500

    campsitesSlice.js

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -1,21 +1,21 @@
-import { CAMPSITES } from '../../app/shared/CAMPSITES';
-import { Col, Row} from "reactstrap";
+import { Col, Row, } from 'reactstrap';
 import CampsiteCard from "./CampsiteCard";
+import { selectAllCampsites } from './campsitesSlice';
 
 const CampsitesList = () => {
-    return (
-        <Row className='ms-auto'>
-            {CAMPSITES.map((campsite) => {
+    const campsites = selectAllCampsites();
+
+    return (     
+        <Row className="ms-auto">
+            {campsites.map((campsite) => {
                 return (
-                    <Col md="5" className='m-4' key={campsite.id}>
-                        <CampsiteCard campsite={campsite}/>
+                    <Col md="5" className="m-4" key={campsite.id} >
+                        <CampsiteCard campsite={campsite} />
                     </Col>
                 );
-
             })}
-
-        </Row>
-    )
-}
+        </Row>  
+    );
+};
 
 export default CampsitesList;
