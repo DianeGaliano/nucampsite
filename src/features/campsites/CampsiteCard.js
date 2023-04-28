fb9954f81fa790b21e6d967026430e447e0a31a8
Diane Galiano <dianegaliano09@gmail.com>
2023-04-27 23:54:04 -0500

    Destructuring assignment

--- src/features/campsites/CampsiteCard.js
+++ src/features/campsites/CampsiteCard.js
@@ -1,20 +1,14 @@
 import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
 
-const CampsiteCard = (props) => {
-        return (
+const CampsiteCard = ({ campsite }) => {
+    const { image, name } = campsite;
+    return (
         <Card>
-              <CardImg 
-                width='100%'
-                src={props.campsite.image}
-                alt={props.campsite.name}
-            /> 
+            <CardImg width='100%' src={image} alt={name} />
             <CardImgOverlay>
-                  <CardTitle>                   
-                     {props.campsite.name}      
-                  </CardTitle>  
+                <CardTitle>{name}</CardTitle>
             </CardImgOverlay>
         </Card>
     );
-}
-
+};
 export default CampsiteCard;
