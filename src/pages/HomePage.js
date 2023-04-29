f9a958e946b5e71e82ac636c59ee5c68db22b0c8
Diane Galiano <dianegaliano09@gmail.com>
2023-04-28 23:39:18 -0500

    React Router

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,7 +5,7 @@ import SubHeader from '../components/SubHeader';
 const HomePage = () => {
     return (
         <Container>
-            <SubHeader current={"Home"}/>
+            <SubHeader current="Home"/>
             <DisplayList />
         </Container>
     );
