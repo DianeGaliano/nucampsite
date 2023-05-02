e75b0f54db30f811c28c5debe786a32c6fb557a4
Diane Galiano <dianegaliano09@gmail.com>
2023-05-01 21:01:40 -0500

    ContactPage

---
+++ src/components/ContactForm.js
@@ -0,0 +1,66 @@
+import { Button, Lable, Col, FormGroup } from "reactstrap";
+import { Formik, Field, Form } from "formik";
+
+const ContactForm = () => {
+    return (
+        <Formik
+        initialValues={{
+            firstName: '',
+            lastName: '',
+            phoneNum: '',
+            email: '',
+            agree: false,
+            contactType: 'By Phone',
+            feedback: ''
+        }}
+    >
+        <Form>
+                <FormGroup row>
+                    <Label htmlFor='firstName' md='2'>
+                        First Name
+                    </Label>
+                    <Col md='10'>
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label htmlFor='lastName' md='2'>
+                        Last Name
+                    </Label>
+                    <Col md='10'>
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label htmlFor='phoneNum' md='2'>
+                        Phone
+                    </Label>
+                    <Col md='10'>
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label htmlFor='email' md='2'>
+                        Email
+                    </Label>
+                    <Col md='10'>
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label>
+                    </Label>
+                    <Col md='4'>
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label htmlFor='feedback' md='2'>
+                        Your Feedback
+                    </Label>
+                    <Col md='10'>
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                </FormGroup>
+            </Form>
+    );
+
+};
+
+export default ContactForm;
