877d91c292b3ffa3e71149d3061aa358e72c14fc
Diane Galiano <dianegaliano09@gmail.com>
2023-05-08 17:14:59 -0500

    Redux campsitesSlice

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -1,4 +1,15 @@
 import { CAMPSITES } from "../../app/shared/CAMPSITES";
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const initialState = {
+    campsitesArray: CAMPSITES
+};
+
+const campsitesSlice = createSlice({
+    name: 'campsites',
+    initialState
+});
 
 export const selectAllCampsites = () => {
     return CAMPSITES;
@@ -11,3 +22,5 @@ export const selectCampsiteById = (id) => {
 export const selectFeaturedCampsite = () => { 
     return CAMPSITES.find((campsite) => campsite.featured);
 };
+
+export const campsitesReducer = campsitesSlice.reducer;
