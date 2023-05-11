bc2f6c9f89a9b942389daf14d419a89299fb102b
Diane Galiano <dianegaliano09@gmail.com>
2023-05-11 16:42:41 -0500

    Redux for All

--- src/features/comments/commentsSlice.js
+++ src/features/comments/commentsSlice.js
@@ -1,8 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
 import { COMMENTS } from '../../app/shared/COMMENTS';
 
+const initialState = {
+    commentsArray: COMMENTS
+};
 
-export const selectCommentsByCampsiteId = (campsiteId) => {
-    return COMMENTS.filter(
+const commentsSlice = createSlice({
+    name: 'comments',
+    initialState
+});
+
+export const commentsReducer = commentsSlice.reducer;
+
+export const selectCommentsByCampsiteId = (campsiteId) => (state) => {
+    return state.comments.commentsArray.filter(
         (comment) => comment.campsiteId === parseInt(campsiteId)
     );
 };
