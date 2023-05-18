e9e9b5fbf2598b7591a7c45dd712370b09c87da9
Diane Galiano <dianegaliano09@gmail.com>
2023-05-17 21:55:40 -0500

    Async Promotions

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { fetchCampsites } from './features/campsites/campsitesSlice';
 import { fetchPartners } from './features/partners/partnersSlice';
+import { fetchPromotions } from './features/promotions/promotionsSlice';
 import './App.css';
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
     useEffect(() => {
         dispatch(fetchCampsites());
         dispatch(fetchPartners());
+        dispatch(fetchPromotions());
     }, [dispatch]);
     
     return (
