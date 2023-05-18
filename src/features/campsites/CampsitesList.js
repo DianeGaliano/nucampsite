e92d33399de46686fff619652ccd3e0b23903752
Diane Galiano <dianegaliano09@gmail.com>
2023-05-17 19:52:30 -0500

    Error and Loading

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -8,6 +8,25 @@ import Loading from '../../components/Loading';
 const CampsitesList = () => {
     const campsites = useSelector(selectAllCampsites);
     console.log('campsites:', campsites);
+
+    const isLoading = useSelector((state) => state.campsites.isLoading);
+    const errMsg = useSelector((state) => state.campsites.errMsg);
+
+    if (isLoading) {
+        return (
+            <Row>
+                <Loading />
+            </Row>
+        );
+    }
+
+    if (errMsg) {
+        return (
+            <Row>
+                <Error errMsg={errMsg} />
+            </Row>
+        );
+    }
     return (
         <Row className='ms-auto'>
             {campsites.map((campsite) => {
